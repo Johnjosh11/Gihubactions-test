8b11dc4c32776f1ced60adff9c698c0f21c6206d
john-sage <john.boothapati@sage.com>
2022-05-20 09:31:54 +0100

    test

--- provider.tf
+++ provider.tf
@@ -2,15 +2,11 @@ terraform {
   required_version = ">0.14.7"
   
   backend "s3" {
-      bucket = "test-gitworkflow"
-      key = "terraform.tfstate"
-      region = "us-east-2"
+    bucket = "test-gitworkflow"
+     key = "terraform.tfstate"
+     region = "us-east-2"
   }
-
-  required_providers {
-    aws = {
-      version = ">= 3.66.0"
-      source  = "hashicorp/aws"
-    }
-  }
+}
+provider "aws" {
+    region = "us-east-2"
 }
